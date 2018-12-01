d4c5928df88c845987c5006fddb6e821a3e0cd09
david <david_gh86@hotmail.es>
2018-12-01 02:32:00 +0100

    fix two half bricks rows

--- src/main/java/Brick.java
+++ src/main/java/Brick.java
@@ -1,6 +1,7 @@
 public enum Brick {
 
-    LAST_BRICK_OF_WALL("■■"), NORMAL_BRICK("■■|"), FIRST_HALF_BRICK("■|"), LAST_HALF_BRICK("|■");
+    LAST_BRICK_OF_WALL("■■"), NORMAL_BRICK("■■|"), FIRST_HALF_BRICK("■|")
+    , LAST_HALF_BRICK("|■"), HALF_BRICK_WITHOUT_MORTER("■");
 
     private String characters;
 
--- src/main/java/RowOfBricksWithHalfBricks.java
+++ src/main/java/RowOfBricksWithHalfBricks.java
@@ -11,12 +11,25 @@ public class RowOfBricksWithHalfBricks extends RowOfBricks {
 
     @Override
     public void buildRow(int numberOfBricks) throws IncorrectNumberOfBricksException {
-        RowOfBricks rowOfBricksWithOnlyCompleteBricks =
-                new RowOfBricksWithOnlyCompleteBricks(numberOfBricks-1);
-        List<Brick> listOfBricksThatAreComplete = rowOfBricksWithOnlyCompleteBricks.getBricks();
         bricks = new ArrayList<>();
         bricks.add(Brick.FIRST_HALF_BRICK);
-        bricks.addAll(listOfBricksThatAreComplete);
+        if (numberOfBricks == 1){
+            bricks.add(Brick.HALF_BRICK_WITHOUT_MORTER);
+        }
+        else {
+            addRemainingBricks(numberOfBricks);
+        }
+    }
+
+    private void addRemainingBricks(int numberOfBricks) throws IncorrectNumberOfBricksException {
+        addCompleteBricks(numberOfBricks);
         bricks.add(Brick.LAST_HALF_BRICK);
+    }
+
+    private void addCompleteBricks(int numberOfBricks) throws IncorrectNumberOfBricksException {
+        RowOfBricks rowOfBricksWithOnlyCompleteBricks =
+                new RowOfBricksWithOnlyCompleteBricks(numberOfBricks - 1);
+        List<Brick> listOfBricksThatAreComplete = rowOfBricksWithOnlyCompleteBricks.getBricks();
+        bricks.addAll(listOfBricksThatAreComplete);
     }
 }
